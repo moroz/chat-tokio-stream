f3eeb536c8717d2f380a0ab0b5b95548f93b518d
Karol Moroz <k.j.moroz@gmail.com>
2022-12-01 00:49:00 +0100

    Implement TCP echo

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use tokio::io::AsyncReadExt;
+use tokio::io::AsyncWriteExt;
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() {
+    let listener = TcpListener::bind("localhost:8080").await.unwrap();
+
+    let (mut socket, _addr) = listener.accept().await.unwrap();
+
+    let mut buffer = [0u8; 1024];
+
+    let bytes_read = socket.read(&mut buffer).await.unwrap();
+
+    socket.write_all(&mut buffer[..bytes_read]).await.unwrap();
 }
