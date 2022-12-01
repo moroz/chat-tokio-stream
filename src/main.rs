331460d2a7197422bda89004b1cfbf1baed7cf8d
Karol Moroz <k.j.moroz@gmail.com>
2022-12-01 01:05:57 +0100

    Get this code to run asynchronously

--- src/main.rs
+++ src/main.rs
@@ -7,21 +7,25 @@ use tokio::net::TcpListener;
 async fn main() {
     let listener = TcpListener::bind("localhost:8080").await.unwrap();
 
-    let (mut socket, _addr) = listener.accept().await.unwrap();
-
-    let (read, mut write) = socket.split();
-
-    let mut reader = BufReader::new(read);
-    let mut line = String::new();
-
     loop {
-        let bytes_read = reader.read_line(&mut line).await.unwrap();
+        let (mut socket, _addr) = listener.accept().await.unwrap();
 
-        if bytes_read == 0 {
-            break;
-        }
+        tokio::spawn(async move {
+            let (read, mut write) = socket.split();
 
-        write.write_all(line.as_bytes()).await.unwrap();
-        line.clear();
+            let mut reader = BufReader::new(read);
+            let mut line = String::new();
+
+            loop {
+                let bytes_read = reader.read_line(&mut line).await.unwrap();
+
+                if bytes_read == 0 {
+                    break;
+                }
+
+                write.write_all(line.as_bytes()).await.unwrap();
+                line.clear();
+            }
+        });
     }
 }
