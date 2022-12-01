20a9d69059185c59ce0cecb4006fa47f583b3b17
Karol Moroz <k.j.moroz@gmail.com>
2022-12-01 01:20:43 +0100

    Make it async

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,18 @@ use tokio::io::AsyncBufReadExt;
 use tokio::io::AsyncWriteExt;
 use tokio::io::BufReader;
 use tokio::net::TcpListener;
+use tokio::sync::broadcast;
 
 #[tokio::main]
 async fn main() {
     let listener = TcpListener::bind("localhost:8080").await.unwrap();
 
+    let (tx, _rx) = broadcast::channel::<String>(10);
+
     loop {
         let (mut socket, _addr) = listener.accept().await.unwrap();
+        let tx = tx.clone();
+        let mut rx = tx.subscribe();
 
         tokio::spawn(async move {
             let (read, mut write) = socket.split();
@@ -17,14 +22,21 @@ async fn main() {
             let mut line = String::new();
 
             loop {
-                let bytes_read = reader.read_line(&mut line).await.unwrap();
+                tokio::select! {
+                    result = reader.read_line(&mut line) => {
+                        if result.unwrap() == 0 {
+                            break;
+                        }
+                        tx.send(line.clone()).unwrap();
+                        line.clear();
+                    }
 
-                if bytes_read == 0 {
-                    break;
+                    result = rx.recv() => {
+                        let msg = result.unwrap();
+
+                        write.write_all(msg.as_bytes()).await.unwrap();
+                    }
                 }
-
-                write.write_all(line.as_bytes()).await.unwrap();
-                line.clear();
             }
         });
     }
